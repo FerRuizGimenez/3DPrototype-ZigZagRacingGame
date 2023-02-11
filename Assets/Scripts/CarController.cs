c74875e99f419c7a549b4191be943582d6b68c11
Fernando Ruiz Gimenez <48037548+FerRuizGimenez@users.noreply.github.com>
2023-02-11 18:15:04 +0100

    #2 Making our car move
    - In principle, the position of the car is moved by adding a velocity to the z-direction of the car itself
    - Change the direction of the car
    - By clicking the rotation on the y-axis of the car changes by 90 degrees, causing it to change direction
    - The car stays still at the beginning, when the player clicks on the screen it starts to move

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    bool movingLeft = true;
+    bool firstInput = true;
+
+    private void Update() 
+    {
+        if(GameManager.instance.gameStarted)
+        {
+            Move();    
+            CheckInput();
+        }
+    }
+    private void Move()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+
+    void CheckInput()
+    {
+        // if first input then ignore
+        if(firstInput)
+        {
+            firstInput = false;
+            return;
+        }
+        
+        if(Input.GetMouseButtonDown(0))
+        {
+            ChangeDirection();
+        }
+    }
+    private void ChangeDirection()
+    {
+        if(movingLeft)
+        {
+            movingLeft = false;
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        } 
+        else
+        {
+            movingLeft = true;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
