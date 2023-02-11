98cd9452c01ec159963145c941b0a6144f33f6a7
Fernando Ruiz Gimenez <48037548+FerRuizGimenez@users.noreply.github.com>
2023-02-11 22:01:08 +0100

    #4 Camera behaviour: follow the car
    - We make the car the target of the camera and follow it
    - First we calculate the initial distance between the camera and the car
    - Then, when the position of the car is updated, we subtract the initial distance so that the camera maintains a distance between the car and the camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    Vector3 distance;
+    public float smoothValue;
+
+    private void Start() 
+    {
+        distance = target.position - transform.position;    
+    }
+
+    private void Update() 
+    {
+        Follow();    
+    }
+
+    private void Follow()
+    {
+        Vector3 currentPos = transform.position;
+        Vector3 targetPos = target.position - distance;
+
+        transform.position = Vector3.Lerp(currentPos, targetPos, smoothValue * Time.deltaTime);
+
+    }
+}
